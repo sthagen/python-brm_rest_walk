d056fe498bfd8c5f14dc59afe4f72d2c4f97cf34
Stefan Hagen <stefan@hagen.link>
2020-08-03 14:03:35 +0200

    Removed shebang line

--- brm_rest_walk/brm_rest_walk.py
+++ brm_rest_walk/brm_rest_walk.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # pylint: disable=expression-not-assigned,line-too-long
 """Walk the REST accessible path tree of some binary repository management system."""
