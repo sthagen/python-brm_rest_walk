cec3189448cf858a0f978690ec5b7f9ef892a483
Stefan Hagen <stefan@hagen.link>
2020-12-05 12:38:25 +0100

    Set up bogus test context

--- tests/context.py
+++ tests/context.py
@@ -1,1 +1,20 @@
+"""Setting up the context for test."""
+import os
+BRM_FS_ROOT = "."
+BRM_SERVER = "https://example.com"
+BRM_API_ROOT = "https://example.com"
+BRM_USER = "none"
+BRM_TOKEN = "0xcafedafe"
 
+
+def init():
+    """Set some environment variables."""
+    os.environ["BRM_FS_ROOT"] = BRM_FS_ROOT
+    os.environ["BRM_SERVER"] = BRM_SERVER
+    os.environ["BRM_API_ROOT"] = BRM_API_ROOT
+    os.environ["BRM_USER"] = BRM_USER
+    os.environ["BRM_TOKEN"] = BRM_TOKEN
+
+
+init()
+reset = init
