859335d97fb5c9230de499795f49b8a08112269f
Stefan Hagen <stefan@hagen.link>
2020-12-05 13:01:17 +0100

    Added negative start token href nonsense page test for parsing of autoindex (digging one level deeper)

--- tests/test_brm_rest_walk.py
+++ tests/test_brm_rest_walk.py
@@ -74,3 +74,9 @@ def test_parse_autoindex_nok_start_token_nonsense():
     message = r"not enough values to unpack \(expected 2, got 1\)"
     with pytest.raises(ValueError, match=message):
         brm.parse_autoindex('<a href="There is nothing useful in here')
+
+
+def test_parse_autoindex_nok_start_token_href_nonsense():
+    message = r"not enough values to unpack \(expected 2, got 1\)"
+    with pytest.raises(ValueError, match=message):
+        brm.parse_autoindex('<a href="maybe">There is nothing useful in here')
