85de5d7be2e83ffc42810082a0ff85436b3921d4
Stefan Hagen <stefan@hagen.link>
2020-12-06 14:26:22 +0100

    Fixed 3/3 tests after refactoring (and flattening) of the node info meta data - all tests pass again

--- tests/test_brm_rest_walk.py
+++ tests/test_brm_rest_walk.py
@@ -623,28 +623,28 @@ def test_tree_walker_ok_tree_edge_leaf_page():
                 '@e': ['a.txt', 'b/'],
                 'a.txt': {
                     '@n': {
-                        '@m': ('a.txt',
-                               '22-Aug-2019 09:53',
-                               '2.50',
-                               'MB'),
-                        'url': 'https://example.com/api/data/a.txt',
+                        'api_ts': '22-Aug-2019 09:53',
+                        'h_size': '2.50',
+                        'h_unit': 'MB',
                         'md5': '921214c14fda7cd320caf04cfa26a224',
+                        'name': 'a.txt',
                         'sha1': '7c6b7b5a662dcf0a21253bc2576d614f6b7fdc9c',
-                        'sha256': 'fd60560f94c1ad21d45e2383f974dd77df582f7336816b7fb367d70ff001fc8f'
+                        'sha256': 'fd60560f94c1ad21d45e2383f974dd77df582f7336816b7fb367d70ff001fc8f',
+                        'url': 'https://example.com/api/data/a.txt'
                     }
                 },
                 'b/': {
                     '@e': ['b.txt'],
                     'b.txt': {
                         '@n': {
-                            '@m': ('b.txt',
-                                   '22-Aug-2020 09:53',
-                                   '1.23',
-                                   'kB'),
-                            'url': 'https://example.com/api/data/b/b.txt',
+                            'api_ts': '22-Aug-2020 09:53',
+                            'h_size': '1.23',
+                            'h_unit': 'kB',
                             'md5': '640ecd5a7cf34cbf8a921b37731db28b',
+                            'name': 'b.txt',
                             'sha1': 'd07cd80af550e403df824d64feb67e34a9fbf020',
-                            'sha256': '98dccf9bba2c9294ffcf7772d9dc72f80580d6c08cae4537dd861faa3c85d25e'
+                            'sha256': '98dccf9bba2c9294ffcf7772d9dc72f80580d6c08cae4537dd861faa3c85d25e',
+                            'url': 'https://example.com/api/data/b/b.txt'
                         }
                     }
                 }
